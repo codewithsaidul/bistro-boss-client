a31d1794c76166017dfb9db89f713818a80354dc
codewithsaidul <codewithsaidul@gmail.com>
2024-05-17 11:29:51 +0600

    Footer Component Added

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,10 +1,13 @@
+
 import { Outlet } from "react-router-dom"
+import Footer from "../Shared/Footer"
 
 
 const Root = () => {
   return (
     <div>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter  } from "react-router-dom";
+
+import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layouts/Root";
 import Home from "../pages/Home/Home";
 
